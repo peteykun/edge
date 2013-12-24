c56fc8eba37a97d283a4536ece4c147ac404ba60
Soham Pal <pt@live.in>
2013-12-24 15:56:43 +0530

    Update gemfile and some stylistic changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,5 @@ class Event < ActiveRecord::Base
   belongs_to :contact1, class_name: 'Contact', foreign_key: :contact1_id
   belongs_to :contact2, class_name: 'Contact', foreign_key: :contact2_id
 
-  has_attached_file :image, styles: { thumb: '100x100' }
+  has_attached_file :image, styles: { thumb: '100x100', tile: '375x192#', flagship: '760' }
 end
