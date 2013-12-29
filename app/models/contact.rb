2328396834206cb3081d0550bf3106e438cd4caf
Soham Pal <pt@live.in>
2013-12-29 17:34:08 +0530

    Change graph API query to https

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,7 @@ class Contact < ActiveRecord::Base
   has_many :contact_categories
 
   def facebook_avatar
-    'http://graph.facebook.com/' + facebook_username + '/picture?width=150&height=150'
+    'https://graph.facebook.com/' + facebook_username + '/picture?width=150&height=150'
   end
 
   def facebook_url
