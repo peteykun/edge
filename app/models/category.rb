106aeb8c4f5b29b6b3420ba3b526990dec2d9cee
Soham Pal <pt@live.in>
2013-12-29 16:54:07 +0530

    Get rid of dropbox storage

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :events
 
-  has_attached_file :black_icon, styles: { small: '30x30' },
-    storage: :dropbox,
-    dropbox_credentials: Rails.root.join('config/dropbox.yml'),
-    path: ':class/:attachment/:id_partition/:style/:filename'
+  has_attached_file :black_icon, styles: { small: '30x30' }
     
-  has_attached_file :white_icon, styles: { small: '30x30' },
-    storage: :dropbox,
-    dropbox_credentials: Rails.root.join('config/dropbox.yml'),
-    path: ':class/:attachment/:id_partition/:style/:filename'
+  has_attached_file :white_icon, styles: { small: '30x30' }
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,5 @@ class Event < ActiveRecord::Base
   belongs_to :contact1, class_name: 'Contact', foreign_key: :contact1_id
   belongs_to :contact2, class_name: 'Contact', foreign_key: :contact2_id
 
-  has_attached_file :image, styles: { thumb: '100x100', tile: '375x192#', flagship: '760' },
-    storage: :dropbox,
-    dropbox_credentials: Rails.root.join('config/dropbox.yml'),
-    path: ':class/:attachment/:id_partition/:style/:filename'
+  has_attached_file :image, styles: { thumb: '100x100', tile: '375x192#', flagship: '760' }
 end
